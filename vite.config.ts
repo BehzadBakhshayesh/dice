0d7f124d66272e8884a37adb4f49066b3b30cffd
Bhzd <bhzd.bakhshayesh@gmail.com>
2026-06-13 14:08:06 +0430

    .

--- vite.config.ts
+++ vite.config.ts
@@ -29,8 +29,28 @@ export default defineConfig({
         scope: '/',
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
+            src: 'pwa-32x32.png',
+            sizes: '32x32',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-48x48.png',
+            sizes: '48x48',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-72x72.png',
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-144x144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-256x256.png',
+            sizes: '256x256',
             type: 'image/png'
           },
           {
